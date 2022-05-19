e00075ea3bdf4fda4f85add2ab651fc3dd067300
pavel-glukhov <glukhov.p@gmail.com>
2022-05-19 18:44:37 +0600

    Added Comments to functions

--- app/car_rent.py
+++ app/car_rent.py
@@ -25,6 +25,13 @@ def clear_booked_cars():
 
 
 async def get_offers(source: str) -> list[dict]:
+    """
+        Эта функция эмулирует асинхронных запрос по сети в сервис каршеринга source.
+         Например source = "yandex" - запрашиваем список свободных автомобилей в сервисе yandex.
+
+        Keyword arguments:
+        source - сайт каршеринга
+        """
     await asyncio.sleep(1)
 
     return [
@@ -37,6 +44,12 @@ async def get_offers(source: str) -> list[dict]:
 
 
 async def get_offers_from_sourses(sources: list[str]) -> list[dict]:
+    """
+    Эта функция агрегирует предложения из списка сервисов по каршерингу
+
+    Keyword arguments:
+    sources - список сайтов каршеринга ["yandex", "belka", "delimobil"]
+    """
     global CURRENT_AGG_REQUESTS_COUNT
 
     if CURRENT_AGG_REQUESTS_COUNT >= MAX_PARALLEL_AGG_REQUESTS_COUNT:
@@ -69,6 +82,39 @@ async def worker_combine_service_offers(
 async def chain_combine_service_offers(
         inbound: Queue, outbound: Queue, **kw
 ):
+    """
+    Запускает N функций worker-ов для обработки данных из очереди inbound и передачи результата в outbound очередь.
+    N worker-ов == WORKERS_COUNT (константа из app/const.py)
+
+    Нужно подобрать такой примитив синхронизации, который бы ограничивал вызов функции get_offers_from_sourses для N воркеров.
+    Ограничение количества вызовов - MAX_PARALLEL_AGG_REQUESTS_COUNT (константа из app/const.py)
+
+    Keyword arguments:
+    inbound: Queue[PipelineContext] - очередь данных для обработки
+    Пример элемента PipelineContext из inbound:
+    PipelineContext(
+        user_id = 1,
+        data = [
+           "yandex.ru",
+           "delimobil.ru",
+           "belkacar.ru",
+        ]
+    )
+
+    outbound: Queue[PipelineContext] - очередь для передачи обработанных данных
+    Пример элемента PipelineContext в outbound:
+    PipelineContext(
+        user_id = 1,
+        data = [
+            {"url": "http://yandex.ru/car?id=1", "price": 1_000, "brand": "LADA"},
+            {"url": "http://yandex.ru/car?id=2", "price": 5_000, "brand": "MITSUBISHI"},
+            {"url": "http://yandex.ru/car?id=3", "price": 3_000, "brand": "KIA"},
+            {"url": "http://yandex.ru/car?id=4", "price": 2_000, "brand": "DAEWOO"},
+            {"url": "http://yandex.ru/car?id=5", "price": 10_000, "brand": "PORSCHE"},
+    ]
+    )
+    """
+
     sem = Semaphore(MAX_PARALLEL_AGG_REQUESTS_COUNT)
     await asyncio.gather(
         asyncio.create_task(
@@ -84,6 +130,40 @@ async def chain_filter_offers(
         price: Optional[int] = None,
         **kw,
 ):
+    """
+    Функция обработывает данных из очереди inbound и передает результат в outbound очередь.
+    Нужно при наличии параметров brand и price - отфильтровать список предожений.
+
+    Keyword arguments:
+    brand: Optional[str] - название бренда по которому фильруем предложение. Условие brand == offer["brand"]
+    price: Optional[int] - максимальная стоимость предложения. Условие price >= offer["price"]
+
+    inbound: Queue[PipelineContext] - очередь данных для обработки
+    Пример элемента PipelineContext из inbound:
+    PipelineContext(
+        user_id = 1,
+        data = [
+            {"url": "http://yandex.ru/car?id=1", "price": 1_000, "brand": "LADA"},
+            {"url": "http://yandex.ru/car?id=2", "price": 5_000, "brand": "MITSUBISHI"},
+            {"url": "http://yandex.ru/car?id=3", "price": 3_000, "brand": "KIA"},
+            {"url": "http://yandex.ru/car?id=4", "price": 2_000, "brand": "DAEWOO"},
+            {"url": "http://yandex.ru/car?id=5", "price": 10_000, "brand": "PORSCHE"},
+    ]
+    )
+
+    outbound: Queue[PipelineContext] - очередь для передачи обработанных данных
+    Пример элемента PipelineContext в outbound:
+    PipelineContext(
+        user_id = 1,
+        data = [
+            {"url": "http://yandex.ru/car?id=1", "price": 1_000, "brand": "LADA"},
+            {"url": "http://yandex.ru/car?id=2", "price": 5_000, "brand": "MITSUBISHI"},
+            {"url": "http://yandex.ru/car?id=3", "price": 3_000, "brand": "KIA"},
+            {"url": "http://yandex.ru/car?id=4", "price": 2_000, "brand": "DAEWOO"},
+            {"url": "http://yandex.ru/car?id=5", "price": 10_000, "brand": "PORSCHE"},
+    ]
+    )
+    """
     while True:
         ctx = await inbound.get()
         ctx.data = [
@@ -98,11 +178,17 @@ async def chain_filter_offers(
 
 
 async def cancel_book_request(user_id: int, offer: dict):
+    """
+    Эмулирует запрос отмены бронирования  авто
+    """
     await asyncio.sleep(1)
     BOOKED_CARS[user_id].remove(offer.get("url"))
 
 
 async def book_request(user_id: int, offer: dict, event: Event) -> dict:
+    """
+    Эмулирует запрос бронирования авто. В случае отмены вызывает cancel_book_request.
+    """
     try:
         BOOKED_CARS[user_id].add(offer.get("url"))
         await asyncio.sleep(1)
@@ -144,6 +230,32 @@ async def worker_book_car(inbound: Queue[PipelineContext],
 async def chain_book_car(inbound: Queue[PipelineContext],
                          outbound: Queue[PipelineContext], **kw
                          ):
+    """
+       Запускает N функций worker-ов для обработки данных из очереди inbound и передачи результата в outbound очередь.
+       Worker должен параллельно вызвать book_request для каждого предложения. Первый отработавший запрос передать в PipelineContext.
+       Остальные запросы нужно отменить и вызвать для них cancel_book_request.
+
+       Keyword arguments:
+       inbound: Queue[PipelineContext] - очередь данных для обработки
+       Пример элемента PipelineContext из inbound:
+       PipelineContext(
+           user_id = 1,
+           data = [
+               {"url": "http://yandex.ru/car?id=1", "price": 1_000, "brand": "LADA"},
+               {"url": "http://yandex.ru/car?id=2", "price": 5_000, "brand": "MITSUBISHI"},
+               {"url": "http://yandex.ru/car?id=3", "price": 3_000, "brand": "KIA"},
+               {"url": "http://yandex.ru/car?id=4", "price": 2_000, "brand": "DAEWOO"},
+               {"url": "http://yandex.ru/car?id=5", "price": 10_000, "brand": "PORSCHE"},
+       ]
+       )
+
+       outbound: Queue[PipelineContext] - очередь для передачи обработанных данных
+       Пример элемента PipelineContext в outbound:
+       PipelineContext(
+           user_id = 1,
+           data = {"url": "http://yandex.ru/car?id=1", "price": 1_000, "brand": "LADA"}
+       )
+       """
     await asyncio.gather(
         asyncio.create_task(
             worker_book_car(inbound, outbound))
@@ -152,6 +264,36 @@ async def chain_book_car(inbound: Queue[PipelineContext],
 
 
 def run_pipeline(inbound: Queue[PipelineContext]) -> Queue[PipelineContext]:
+    """
+        Необходимо создать asyncio.Task для функций:
+        - chain_combine_service_offers
+        - chain_filter_offers
+        - chain_book_car
+
+        Создать необходимые очереди для обмена данными между звеньями (chain-ами)
+        -> chain_combine_service_offers -> chain_filter_offers -> chain_book_car ->
+
+        Вернуть outbound очередь для звена chain_book_car
+
+        Keyword arguments:
+        inbound: Queue[PipelineContext] - очередь данных для обработки
+        Пример элемента PipelineContext из inbound:
+        PipelineContext(
+            user_id = 1,
+            data = [
+               "yandex.ru",
+               "delimobil.ru",
+               "belkacar.ru",
+            ]
+        )
+
+        -> Queue[PipelineContext] - очередь для передачи обработанных данных
+        Пример элемента PipelineContext из возвращаемой очереди:
+        PipelineContext(
+            user_id = 1,
+            data = {"url": "http://yandex.ru/car?id=1", "price": 1_000, "brand": "LADA"}
+        )
+        """
     queue_combine_service = Queue()
     queue_filter_offers = Queue()
     queue_book_car = Queue()
